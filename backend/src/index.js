8fb5b9ea78a66528b9062ee3218b846c45f1b57a
Shivam Chopra <parthasus7973@gmail.com>
2025-02-26 23:03:04 +0530

    initialized

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { connectDb } from './lib/db.js';
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: 'http://localhost:5173/',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: [
+    'Content-Type',
+    'authorization',
+    'pragma',
+    'cache-control',
+  ],
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDb();
+});
+
